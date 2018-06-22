581f4150d536b4b892e3da87c0dd33b5596bfa8a
damsiem <damthansiem@gmail.com>
2018-06-22 12:27:59 +0700

    build.sh
    create build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env bash
+SRC_DIR="ios"
+NODE_MODULES="node_modules"
+set -ex
+
+    # cd "ios"
+    export PATH=$PATH:~./fastlane/bin:/usr/local/bin:/~./npm/bin:/usr/local/bin
+
+    if [ ! -d ${NODE_MODULES} ]; then
+        # echo "Project: ${NODE_MODULES}"
+        npm install
+        npm link
+    fi
+
+    export LC_ALL=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    # fastlane -v
+    # npm -v
+    npm run d-b-a
+
+
+
